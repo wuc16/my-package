aad60b0cdbc0cca8a8228f0f6035f13c854478bb
Chen Wu <wuc16@miamioh.edu>
2022-05-04 09:31:07 -0400

    udpate some changes

--- src/Hello.php
+++ src/Hello.php
@@ -1,7 +1,7 @@
 <?php 
 
 /*
- * (c) Your Name <your email>
+ * (c) Chen Wu wuc16@miamioh.edu
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
